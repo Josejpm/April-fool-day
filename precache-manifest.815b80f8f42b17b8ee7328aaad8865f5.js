407c906f254887f85e0a5825dbd86a22fa4be333
josejpm <josejpm@gmail.com>
2020-09-06 02:10:55 -0300

    Updates

---
+++ precache-manifest.815b80f8f42b17b8ee7328aaad8865f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f749f7bfe844c86ea232fcab516436c0",
+    "url": "/April-fool-day/index.html"
+  },
+  {
+    "revision": "c4a380d6ec8f5f51484f",
+    "url": "/April-fool-day/static/css/main.fd5293e0.chunk.css"
+  },
+  {
+    "revision": "7e126619024f2a125952",
+    "url": "/April-fool-day/static/js/2.532ebd7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/April-fool-day/static/js/2.532ebd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a380d6ec8f5f51484f",
+    "url": "/April-fool-day/static/js/main.4937aeaa.chunk.js"
+  },
+  {
+    "revision": "da540c9a8e7c98c5e4cd",
+    "url": "/April-fool-day/static/js/runtime-main.48305954.js"
+  },
+  {
+    "revision": "e5c28fa067b50820e95a98ab4881f160",
+    "url": "/April-fool-day/static/media/asfalt.e5c28fa0.png"
+  }
+]);
