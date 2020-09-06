ae708f24691e70c2ed120751769735a50a54e72a
josejpm <josejpm@gmail.com>
2020-09-06 01:38:13 -0300

    Production build done

---
+++ build/precache-manifest.65f4a99cca2af40ce4d90469bf34b33e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f86cbb4e827ba3ccc27dd79ef165820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc50492f39c3af56cf0f",
+    "url": "/static/css/main.0c48e4ab.chunk.css"
+  },
+  {
+    "revision": "7e126619024f2a125952",
+    "url": "/static/js/2.532ebd7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.532ebd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc50492f39c3af56cf0f",
+    "url": "/static/js/main.cc7bb542.chunk.js"
+  },
+  {
+    "revision": "521d542f99dd5f1f56ad",
+    "url": "/static/js/runtime-main.8b64c7be.js"
+  },
+  {
+    "revision": "e5c28fa067b50820e95a98ab4881f160",
+    "url": "/static/media/asfalt.e5c28fa0.png"
+  }
+]);
